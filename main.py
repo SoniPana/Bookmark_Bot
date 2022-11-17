e2868aefbeccfabd0465dca89600812a64b5a667
Geusen <89836078+Geusen@users.noreply.github.com>
2022-11-17 21:50:52 +0900

    [update]status変更

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ handler = StreamHandler()
 format = Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
 handler.setFormatter(format)
 logger.addHandler(handler)
+logger.info('set up was finished.(logging)')
 
 @tasks.loop(seconds=10)
 async def loop():
@@ -31,8 +32,8 @@ async def loop():
 async def on_ready():
     await tree.sync(guild=guild)
     loop.start()
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.competing, name="a movie"))
-    logger.info(f'set up was finished. (ver={discord.__version__})')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.competing, name=f'{str(len(client.guilds))} サーバー'))
+    logger.info(f'set up was finished. (discord.py ver = {discord.__version__})')
 
 @tree.command(name='bookmark', description='見やすい', guild=guild)
 @app_commands.describe(url='メッセージURL', title='タイトル', memo='メモ', color='色指定')
